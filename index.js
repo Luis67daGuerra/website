3d56102eb223126e0a6fe54024b1c997aa3f57e9
Luis67daGuerra <steelbrills@gmail.com>
2025-10-02 08:57:06 -0300

    pare de ser chato

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function mudar_conteudo_elemento(){
-   document.getElementById("demo").innerHTML = "O clique está funcionando";
+function mudar_cor_elemento(){
+      const paragrafo = document.querySelector('#paragrafo')
+      paragrafo.addEventListener('click', ()=> {
+      const elemento_paragrafo = document.querySelector('#paragrafo');
+      elemento_paragrafo.style.color = 'red';
+ });
+}
 
+function mudar_conteudo_elemento(){
+   document.getElementById("demo").innerHTML = "O clique está funcionando"
 }
