0bc3aedeaa85275d9659a9c693ee9f489a903db2
Luis67daGuerra <steelbrills@gmail.com>
2025-10-23 07:41:12 -0300

    mar

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function mudar_cor_elemento(){
-      const paragrafo = document.querySelector('#paragrafo')
-      paragrafo.addEventListener('click', ()=> {
-      const elemento_paragrafo = document.querySelector('#paragrafo');
-      elemento_paragrafo.style.color = 'purple';
- });
+function mudar_texto_elemento() {
+
+  document.getElementById("demo").innerHTML = "O clique está funcionando"
+
 }
 
-function mudar_conteudo_elemento(){
-   document.getElementById("demo").innerHTML = "O clique está funcionando"
+function mudar_cor_elemento(){
+
+  const paragrafo = document.querySelector('#paragrafo');
+
+  paragrafo.addEventListener('click', ()=> {
+
+    const elemento_paragrafo = document.querySelector('#paragrafo');
+
+    elemento_paragrafo.style.color = 'red';
+
+  });
+
+}
+
+function mudar_estilo_textual(){
+
+  document.getElementById("elemento_strong").style.textDecorationLine = "line-through";
 }
