1abe2792132f83c3456852dc510173f596c103fb
Luis67daGuerra <steelbrills@gmail.com>
2025-10-30 08:05:32 -0300

    Goku solos

---
+++ paragrafos.js
@@ -0,0 +1,14 @@
+function mudar_estilo(){
+    document.getElementById("p1").style.textDecorationLine = "overline";
+    document.getElementById("p1").style.textDecorationStyle = "dashed";
+    document.getElementById("p1").style.textTransform = "lowercase";
+    document.getElementById("p1").style.textAlign = "right";
+    document.getElementById("p1").style.textShadow = "3px 3px 3px black";
+    document.getElementById("p1").style.textDecorationThickness = "0.2em";
+    document.getElementById("p1").style.textDecorationColor = "red";
+    document.getElementById("p1").style.textIndent = "50px";
+
+}
+function mudar_estilo2(){
+document.getElementById("p2").style.textTransform = "uppercase";
+}
